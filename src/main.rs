84a5fc0a284669e0b211fe34b2a9de2e022e3a4b
Peter Rong <PeterRong96@gmail.com>
2020-04-11 23:51:19 -0700

    trying to understand where went wrong
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::error::Error;
+use std::process;
+use std::fs::File;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Transaction {
+    movie_id: u64,
+    customer_id: u64,
+    rating: u8,
+    date: String,
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    let file = File::open("./data/train.csv").unwrap();
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let Transaction: Transaction = result?;
+        println!("{:?}", Transaction);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
